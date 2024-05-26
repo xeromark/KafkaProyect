a223f2bf0ee8adbb7348c3f13c046b8e19014ec2
Omar Marca <122169269+xeromark@users.noreply.github.com>
2024-05-26 02:08:57 +0000

    Co-authored-by: N0m4d <nomadstar@users.noreply.github.com>

--- api.py
+++ api.py
@@ -8,23 +8,28 @@ pedidos = []
 def index():
     return "¡Bienvenido a la aplicación de pedidos!"
 
-@app.route("/data")
+
+
+@app.route("/data", methods=['POST'])
 def get_pedido():
     data = { "Nombre": "id_data1", "Precio": "Tilin"}
 
     return jsonify(data), 200
 
 
+
+"""
 @app.route("/data", methods = ['POST'])
 def crear_pedido():
     data = request.get_data().decode("utf-8")
-    pedidos.append(data)
+    #pedidos.append(data)
     
     return data, 201
 
 @app.route("/data/all", methods=['GET'])
 def get_all_pedidos():
     return pedidos, 200
+"""
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
