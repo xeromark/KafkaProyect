3cf559928bb63d0d2b9b19170b9892f8eae5fe22
N0m4d <108416813+nomadstar@users.noreply.github.com>
2024-05-26 20:16:19 -0400

    Graphic

--- metrics.py
+++ metrics.py
@@ -1,7 +1,32 @@
+# Fuente: https://pythonprogramming.net/legends-titles-labels-matplotlib-tutorial/
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
-plt.style.use('_mpl-gallery')
+y = []
+for _ in range(50):
+    #sent, received = time.time(), 0
+    #time.sleep(np.random.randint(0, 900) / 1000)
+    #received = time.time()
+    #delta = received - sent
+    delta = np.random.randint(0,5000) / 1000
+    y.append(delta)
+
+# Calculate percentiles
+percentiles = np.percentile(y, [50, 75, 95, 100])
 
 
+# plot
+plt.plot([50, 75, 95, 100], percentiles, label='First Line')
+plt.ylim(0, 5)
 
+# Add percentiles to the plot
+
+
+plt.xlabel('Percentil')
+plt.ylabel('Latencia en m/s')
+plt.title('Interesting Graph\nCheck it out')
+plt.legend()
+plt.show()
+
+plt.show()
