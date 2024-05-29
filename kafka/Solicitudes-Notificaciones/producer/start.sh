7bc413f844ebcb2f70fc0b733bb794f2c73dfb43
N0m4d <108416813+nomadstar@users.noreply.github.com>
2024-05-28 22:51:08 -0400

    Ayuda

---
+++ kafka/Solicitudes-Notificaciones/producer/start.sh
@@ -0,0 +1,11 @@
+# Made with ChatGPT
+#!/bin/bash
+chmod +x start.sh
+# Actualizar pip
+pip install --upgrade pip
+
+# Instalar los requisitos
+pip install -r dependencies.txt
+
+# Ejecutar el script Python
+python producer.py
