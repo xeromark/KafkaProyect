cf682e1221c412359693328c9f4bf197bc9b0d3c
N0m4d <108416813+nomadstar@users.noreply.github.com>
2024-05-30 20:31:16 -0400

    creación de buffer y reorganización PD: tuvieja

---
+++ kafka/Solicitudes/producer.py
@@ -0,0 +1,64 @@
+from flask import Flask, jsonify, request
+from kafka import KafkaProducer
+import json
+import os
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    with open('buffer.txt', 'w') as file:
+        file.write('0')
+        pass
+    return "¡Bienvenido a la aplicación de pedidos!"
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092') # Arreglar!
+# Check if buffer.txt exists
+
+    
+def ParseToJson(linea):     # Estructura el string como un json, retorna un string con formato json
+    elementos = linea.rstrip().split("|")
+    data = {}
+    with open('buffer.txt', 'r') as file:
+        id = int(file.read())
+        id+=1
+    with open('buffer.txt', 'w') as file:
+        file.write(str(id))
+    data["id"] = id
+    data["nombre"] = elementos[0]
+    data["precio"] = elementos[1]
+
+    
+    return json.dumps(data)
+
+@app.route("/data", methods = ['POST'])
+def crear_pedido():
+
+    data = request.get_data().decode("utf-8") # Pasamos el archivo a texto plano
+
+    producer.send('pedidos', ParseToJson(data).encode('utf-8')) # Envia la informacion
+    producer.flush()
+    return data, 201
+
+
+
+
+
+
+def extraerDatos():
+    # Abre el archivo en modo lectura
+    with open('pedidos', 'r') as archivo:
+        # Lee cada línea del archivo e imprímela
+        c = 0
+        for linea in archivo:
+            if c<2:
+                producer.send('pedidos', ParseToJson(linea).encode('utf-8'))
+                producer.flush()
+                c+=1
+            else:
+                break
+#extraerDatos()
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
+
+    
